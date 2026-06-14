8e7fcff3ae07b7021237027345830b2f4e44995b
Sam <111183258+Hamsu-dev@users.noreply.github.com>
2026-06-14 23:24:51 +1200

    Time: 9 ms (7.68%) | Memory: 11 MB (8.39%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -3,11 +3,9 @@
 class Solution {
 public:
     bool isPalindrome(int x) {  
-    string str = to_string(x);
-    string og = str;
+    string str = to_string(x), og = str;
     reverse(str.begin(), str.end());
-    if (str == og)
-    {
+    if (str == og){
         return true;
     }
     return 0;
