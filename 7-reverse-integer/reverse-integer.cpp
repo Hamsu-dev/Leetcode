7d5e9d8f88d1c036d7e1c553b49041fac6a7be2e
Sam <111183258+Hamsu-dev@users.noreply.github.com>
2026-06-15 20:52:20 +1200

    Time: 0 ms (100.00%) | Memory: 9.4 MB (5.29%) - LeetSync

---
+++ 7-reverse-integer/reverse-integer.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+class Solution {
+public:
+    int reverse(int x) {
+        // just want digits only
+
+        string str = to_string(x);
+        string og;
+
+        string limit = (x < 0) ? "2147483648" : "2147483647";
+
+        for (char c : str)
+        {
+            if (isdigit(c))
+            {
+                og += c;
+            }
+        }
+
+        // Now og is digits only
+        std::reverse(og.begin(), og.end());
+
+        if (og.length() > limit.length() || (og.length() == limit.length() && og > limit))
+        {
+            return 0;
+        }
+
+        int numbers = stoi(og);
+        return (x < 0) ? -numbers : numbers;
+    }
+};
