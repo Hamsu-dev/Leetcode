082658d00164558904107f18cdaa380a4d974b89
Sam <111183258+Hamsu-dev@users.noreply.github.com>
2026-06-15 19:19:36 +1200

    Time: 0 ms (100.00%) | Memory: 10.8 MB (5.22%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -6,7 +6,12 @@ public:
     bool isPalindrome(string s) {
         string final_str = "";
         for (int i = 0; i < s.size(); i++)
-            if (isalnum(s[i])) final_str += tolower(s[i]); 
+        {
+            if (isalnum(s[i]))
+            {
+                final_str += tolower(s[i]); 
+            } 
+        }
         string og = final_str;
         reverse(final_str.begin(), final_str.end());
         return (og == final_str) ? true : false;
