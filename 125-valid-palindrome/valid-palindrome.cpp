d52892d2e712b15f9e299d2b347208c0b1c65de3
Sam <111183258+Hamsu-dev@users.noreply.github.com>
2026-06-15 19:16:04 +1200

    Time: 0 ms (100.00%) | Memory: 10.7 MB (8.25%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -4,11 +4,7 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        // convert all upper to lower case
-        // removing all non alphanumeric characters remove everythign but characters and letters
-
         string final_str = "";
-
         for (int i = 0; i < s.size(); i++)
         {
             if (isalnum(s[i]))
@@ -16,10 +12,8 @@ public:
                 final_str += tolower(s[i]); 
             }
         }
-
         string og = final_str;
         reverse(final_str.begin(), final_str.end());
-
         return (og == final_str) ? true : false;
     }
 };
