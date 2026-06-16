f325575766f621facda385f0cc7fed64cfb44f98
Sam <111183258+Hamsu-dev@users.noreply.github.com>
2026-06-16 20:23:49 +1200

    Time: 12 ms (18.03%) | Memory: 13.4 MB (18.01%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -4,9 +4,7 @@ class Solution {
 public:
     int romanToInt(string s) {
         map<char, int> roman = { {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000} };
-
         int final = 0;
-
         // Input: s = "MCMXCIV"
         for (int i = 0; i < s.size(); i++)
         {
