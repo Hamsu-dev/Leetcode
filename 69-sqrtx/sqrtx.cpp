a95e0527a5260281eeb3c7feaace814cd5fe5283
Sam <111183258+Hamsu-dev@users.noreply.github.com>
2026-06-21 17:11:04 +1200

    Time: 0 ms (100.00%) | Memory: 8.5 MB (49.01%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -3,8 +3,7 @@ public:
     int mySqrt(int x) {
         int L = 0;
         int R = x;
-        int val;
-
+        int value;
         while (L <= R)
         {
             long long mid = L + (R - L) / 2; 
@@ -15,13 +14,13 @@ public:
             if (mid * mid < x) 
             {
                 L = mid + 1;
-                val = mid;
+                value = mid;
             }
             else
             {
                 R = mid - 1;
             }
         }
-        return val;
+        return value;
     }
 };
